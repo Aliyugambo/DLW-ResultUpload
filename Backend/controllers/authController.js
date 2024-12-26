9a8c3e193e6ad1332cf061aafcb94ce6a47d4961
Aliyugambo <gamboaliyu00@gmail.com>
2024-12-26 13:13:18 +0100

    added update

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
+require('dotenv').config()
 // Login endpoint for the admin
 exports.adminLogin = async (req, res) => {
     const { username, password } = req.body;
-    if (username === 'admin' && password === 'admin123') {
+    if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
       const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
       res.json({ token });
     } else {
